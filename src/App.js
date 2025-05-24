cd776ad362ab4971f345611d50c50156c639b178
Taariq Solomons <taariq@soloforce.co.za>
2025-05-24 22:21:01 +0200

    Upload working QA/QC app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>QA/QC Web App</h1>
+      <p>This is a working version deployed on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
